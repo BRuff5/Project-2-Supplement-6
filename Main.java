633c3f7d380003315ec2184c583fcb1b3e836892
BLR <bruff@Macky-Max.local>
2024-12-05 19:32:20 -0500

    First commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+/**
+* Function to general a random list of integers where the size of the list is a parameter
+* Function to take a list of integers and return a map where each integer is a key and the number of occurrences for each integer is the value.
+* Function to sort a list in ascending order
+*/
+
+public class Main{
+
+}
